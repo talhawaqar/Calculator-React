4429e33da0343e106261007f522ffeda30d2c97f
Muhammad Talha waqar <talhawaqar007@gmail.com>
2021-06-10 12:30:15 +0500

    Fix errors

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
 
-const Calculator = () => (
-  <>
-    <Display />
-    <ButtonPanel />
-  </>
-);
+const Calculator = () => {
+  const [calcData, setCalcData] = useState({ total: null, next: null, operation: null });
+
+  const handleClick = (buttonName) => {
+    const btnName = buttonName;
+    let tempData = calcData;
+    tempData = calculate(btnName, tempData);
+    setCalcData(tempData);
+  };
+
+  const displayResult = () => {
+    let result = '';
+    let { total, next, operation } = calcData;
+    if ((total === null) && (next === null)) {
+      result = '0';
+    } else {
+      total = total === null ? '' : total;
+      next = next === null ? '' : next;
+      operation = operation === null ? '' : operation;
+
+      result = `${total} ${operation} ${next}`;
+    }
+
+    return result;
+  };
+
+  return (
+    <>
+      <Display result={displayResult()} />
+      <ButtonPanel handleClick={handleClick} />
+    </>
+  );
+};
 
 export default Calculator;
